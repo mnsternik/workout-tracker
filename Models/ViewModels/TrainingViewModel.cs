9cecded99ff377ec4177ce7391f95ad2900888bd
mnsternik <mnsternik@gmail.com>
2024-11-18 22:34:49 +0100

    Id added

--- Models/ViewModels/TrainingViewModel.cs
+++ Models/ViewModels/TrainingViewModel.cs
@@ -4,10 +4,14 @@ namespace WorkoutTracker.Models.ViewModels
 {
     public class TrainingViewModel
     {
+        public int? Id { get; set; }
+
         [StringLength(90, MinimumLength = 2)]
         [Required]
         public string Name { get; set; }
 
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
 
         [StringLength(300, MinimumLength = 10)]
         public string? Description { get; set; }
