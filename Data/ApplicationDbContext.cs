b50d20b66b5d66ced4ea81bc70cd5dd940163e53
mnsternik <mnsternik@gmail.com>
2024-11-20 23:23:28 +0100

    ApplicationUser class applied

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using WorkoutTracker.Models;
 
 namespace WorkoutTracker.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
