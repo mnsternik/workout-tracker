37021f9bfdd867c0161cabbd540eac2c9a2d0e1f
mnsternik <mnsternik@gmail.com>
2024-11-26 16:24:13 +0100

    ITrainingMapper created

---
+++ Services/Interfaces/ITrainingMapper.cs
@@ -0,0 +1,12 @@
+﻿using WorkoutTracker.Models.ViewModels;
+using WorkoutTracker.Models;
+
+namespace WorkoutTracker.Services.Interfaces
+{
+    public interface ITrainingMapper
+    {
+        public Training MapToTraining(TrainingViewModel viewModel, string? userId);
+        public Training MapToTraining(TrainingViewModel viewModel, Training existingTraining);
+        TrainingViewModel MapToViewModel(Training training);
+    }
+}
