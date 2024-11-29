24268d1dccc3839631f77f917358b53bccd88361
mnsternik <mnsternik@gmail.com>
2024-11-29 15:27:57 +0100

    Trainings type changed to PaginatedList

---
+++ Models/ViewModels/TrainingsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WorkoutTracker.Utilities;
+
+namespace WorkoutTracker.Models.ViewModels
+{
+    public class TrainingsListViewModel
+    {
+        public PaginatedList<Training>? Trainings { get; set; }
+
+        public string? SearchString { get; set; } 
+    }
+}
