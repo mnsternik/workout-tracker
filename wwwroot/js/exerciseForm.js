52fb214757a12d0f8040af3e895df2ca9535a631
mnsternik <mnsternik@gmail.com>
2024-12-09 15:37:42 +0100

    Js code moved to seperate file

---
+++ wwwroot/js/exerciseForm.js
@@ -0,0 +1,55 @@
+﻿function exerciseForm(initialExercises) {
+    return {
+        exercises: initialExercises || [],
+
+        updateValidation() {
+            setTimeout(() => {
+                $('#trainingForm')
+                    .removeData("validator")
+                    .removeData("unobtrusiveValidation");
+                $.validator.unobtrusive.parse('#trainingForm');
+            }, 0);
+        },
+
+        addExercise(type) {
+            this.exercises.push({
+                name: '',
+                description: '',
+                type: type,
+                sets: type === 0
+                    ? [{ repetitions: '', weight: '', exerciseType: type }]
+                    : type === 1
+                        ? [{ duration: '', distance: '', exerciseType: type }]
+                        : [{ duration: '', exerciseType: type }]
+            });
+            this.updateValidation();
+        },
+
+        removeExercise(index) {
+            this.exercises.splice(index, 1);
+            this.updateValidation();
+        },
+
+        addSet(exercise) {
+            if (exercise.type === 0) {
+                exercise.sets.push({ repetitions: '', weight: '', exerciseType: exercise.type });
+            } else if (exercise.type === 1) {
+                exercise.sets.push({ duration: '', distance: '', exerciseType: exercise.type });
+            } else if (exercise.type === 2) {
+                exercise.sets.push({ duration: '', exerciseType: exercise.type });
+            }
+            this.updateValidation();
+        }
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    $('#trainingForm').on('submit', function () {
+        setTimeout(() => {
+            $('#trainingForm')
+                .removeData("validator")
+                .removeData("unobtrusiveValidation");
+            $.validator.unobtrusive.parse('#trainingForm');
+        }, 0);
+    });
+});
