ba6663625bf57aed3473060f3f802d0a3294ac40
mnsternik <mnsternik@gmail.com>
2025-04-24 16:53:54 +0200

    Auth related DTOs created

---
+++ DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutTracker.Api.DTOs.Auth
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutTracker.Api.DTOs.Auth
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+        public string DisplayName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
