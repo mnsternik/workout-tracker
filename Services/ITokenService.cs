b3f6436b2c91b10d28680c2487cdf159e03bbc45
mnsternik <mnsternik@gmail.com>
2025-04-24 16:53:40 +0200

    JWT Token service created

---
+++ Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using WorkoutTracker.Api.DTOs.Auth;
+using WorkoutTracker.Api.Models;
+
+namespace WorkoutTracker.Api.Services
+{
+    public interface ITokenService
+    {
+        LoginResponseDto GenerateToken(ApplicationUser user);
+    }
+}
---
+++ Services/JwtTokenService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WorkoutTracker.Api.DTOs.Auth;
+using WorkoutTracker.Api.Models;
+
+namespace WorkoutTracker.Api.Services
+{
+    public class JwtTokenService : ITokenService
+    {
+        private readonly IConfiguration  _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public LoginResponseDto GenerateToken(ApplicationUser user)
+        {
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            // TODO: Consider adding roles to the token claims
+
+            var jwtSettings = _configuration.GetSection("Jwt"); 
+            var jwtAuthSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var tokenValidityInMinutes = int.Parse(jwtSettings["DurationInMinutes"] ?? "60");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(authClaims),
+                Expires = DateTime.UtcNow.AddMinutes(tokenValidityInMinutes),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"],
+                SigningCredentials = new SigningCredentials(jwtAuthSigningKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHanlder = new JwtSecurityTokenHandler();
+            var token = tokenHanlder.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHanlder.WriteToken(token);
+
+            return new LoginResponseDto
+            {
+                Token = jwtToken,
+                Expiration = token.ValidTo
+            };
+        }
+    }
+}
