dc5db602e04f99d8a7cb760e8353386458d999af
mnsternik <mnsternik@gmail.com>
2025-04-24 16:53:16 +0200

    AuthController created

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutTracker.Api.DTOs.Auth;
+using WorkoutTracker.Api.Models;
+using WorkoutTracker.Api.Services;
+
+namespace WorkoutTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITokenService _tokenService; 
+
+        public AuthController(UserManager<ApplicationUser> userManager, ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            var emailExists = await _userManager.FindByEmailAsync(model.Email);
+            if (emailExists != null)
+                return StatusCode(StatusCodes.Status409Conflict, new { Status = "Error", Message = "Email already exists" });
+
+            ApplicationUser user = new()
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                DisplayName = model.DisplayName,
+                SecurityStamp = Guid.NewGuid().ToString(), // Ważne dla unieważniania tokenów przy zmianie hasła itp.
+            }; 
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { Status = "Error", Message = $"User creation failed. Errors: {string.Join(", ", errors)}" });
+            }
+
+            return StatusCode(StatusCodes.Status201Created, new { Status = "Success", Message = "User created successfully!" });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Email);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenInfo = _tokenService.GenerateToken(user);
+                return Ok(tokenInfo); 
+            }
+
+            return Unauthorized(new { Status = "Error", Message = "Invalid email or password" });
+        }
+    }
+}
---
+++ Controllers/TrainingSessionsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkoutTracker.Api.Data;
+using WorkoutTracker.Api.Models;
+
+namespace WorkoutTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainingSessionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrainingSessionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TrainingSessions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TrainingSession>>> GetTrainingSessions()
+        {
+            return await _context.TrainingSessions.ToListAsync();
+        }
+
+        // GET: api/TrainingSessions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TrainingSession>> GetTrainingSession(int id)
+        {
+            var trainingSession = await _context.TrainingSessions.FindAsync(id);
+
+            if (trainingSession == null)
+            {
+                return NotFound();
+            }
+
+            return trainingSession;
+        }
+
+        // PUT: api/TrainingSessions/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrainingSession(int id, TrainingSession trainingSession)
+        {
+            if (id != trainingSession.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trainingSession).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrainingSessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TrainingSessions
+        [HttpPost]
+        public async Task<ActionResult<TrainingSession>> PostTrainingSession(TrainingSession trainingSession)
+        {
+            _context.TrainingSessions.Add(trainingSession);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTrainingSession), new { id = trainingSession.Id }, trainingSession);
+        }
+
+        // DELETE: api/TrainingSessions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrainingSession(int id)
+        {
+            var trainingSession = await _context.TrainingSessions.FindAsync(id);
+            if (trainingSession == null)
+            {
+                return NotFound();
+            }
+
+            _context.TrainingSessions.Remove(trainingSession);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TrainingSessionExists(int id)
+        {
+            return _context.TrainingSessions.Any(e => e.Id == id);
+        }
+    }
+}
