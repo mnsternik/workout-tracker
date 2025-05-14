3802a3452d3c1878d70f6dbdfa58f8b333baae48
Marcin Sternik <mnsternik@gmail.com>
2025-05-14 22:05:53 +0200

    Models configured

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,9 +11,60 @@ namespace WorkoutTracker.Api.Data
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TrainingSession>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Notes).HasMaxLength(1000);
+                entity.Property(e => e.UserId).IsRequired();
+
+                entity.HasMany(e => e.Exercises)
+                    .WithOne(e => e.TrainingSession)
+                    .HasForeignKey(e => e.TrainingSessionId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<TrainingExercise>(entity =>
+            {
+                entity.Property(e => e.OrderInSession).IsRequired();
+
+                entity.HasOne(e => e.Exercise)
+                  .WithMany()
+                  .HasForeignKey(e => e.ExerciseId);
+
+                entity.HasMany(e => e.Sets)
+                  .WithOne(s => s.TrainingExercise)
+                  .HasForeignKey(s => s.TrainingExerciseId)
+                  .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+            modelBuilder.Entity<TrainingSet>(entity =>
+            {
+                entity.Property(e => e.OrderInExercise).IsRequired();
+                entity.Property(e => e.WeightKg).HasColumnType("decimal(6,2)");
+            });
+
+            modelBuilder.Entity<Exercise>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Description).IsRequired();
+
+                entity.Property(e => e.ExerciseType)
+                      .HasConversion<string>(); 
+
+                entity.Property(e => e.Equipment)
+                      .HasConversion<string>(); 
+
+                entity.Property(e => e.DifficultyLevel)
+                      .HasConversion<string>();
+
+                // TODO: Fix it
+                //entity.HasMany(e => e.MuscleGroups) 
+                //      .WithMany();
+            });
         }
 
         public DbSet<TrainingSession> TrainingSessions { get; set; }
