f7b3a9371f5ffbfafaec6badaf8ed606afab45e8
Marcin Sternik <mnsternik@gmail.com>
2025-05-28 13:50:11 +0200

    EstimatedDurationMinutes property changed to DurationMinutes

--- Models/TrainingSession.cs
+++ Models/TrainingSession.cs
@@ -7,7 +7,7 @@
         public string? Notes { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public int? DifficultyRating { get; set; }
-        public int? EstimatedDurationMinutes { get; set; }
+        public int? DurationMinutes { get; set; }
         public string UserId { get; set; } = string.Empty;
         public ApplicationUser User { get; set; } = null!;
         public ICollection<TrainingExercise> Exercises { get; set; } = [];
