b2670152d63b24b54cb674596a0f0132ee98f8f8
Marcin Sternik <mnsternik@gmail.com>
2025-05-28 13:49:37 +0200

    Filtering traning sessions added

--- Controllers/TrainingSessionsController.cs
+++ Controllers/TrainingSessionsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WorkoutTracker.Api.Data;
 using WorkoutTracker.Api.DTOs.Training.TrainingSession;
+using WorkoutTracker.Api.DTOs.TrainingSession.TrainingSession;
 using WorkoutTracker.Api.Models;
 using WorkoutTracker.Api.Utilities;
 
@@ -26,16 +27,69 @@ namespace WorkoutTracker.Api.Controllers
 
         // GET: api/TrainingSessions
         [HttpGet]
-        public async Task<ActionResult<PaginatedList<TrainingSessionReadDto>>> GetTrainingSessions(int pageIndex = 1, int pageSize = 10)
+        public async Task<ActionResult<PaginatedList<TrainingSessionReadDto>>> GetTrainingSessions([FromQuery] TrainingSessionQueryParameters queryParams)
         {
+            // Base IQueryable for training sessions
             var query = _context.TrainingSessions
+                .Include(t => t.User)
                 .Include(t => t.Exercises)
-                .ThenInclude(te => te.Sets)
+                    .ThenInclude(te => te.Exercise)
+                        .ThenInclude(e => e.MuscleGroupsLinks)
+                .Include(t => t.Exercises)
+                    .ThenInclude(te => te.Sets)
                 .AsNoTracking();
-                
+
+            // Filtering
+            if (!string.IsNullOrEmpty(queryParams.UserId))
+            {
+                query = query.Where(t => t.UserId == queryParams.UserId);
+            }
+
+            if (!string.IsNullOrEmpty(queryParams.DisplayName))
+            {
+                query = query.Where(t => t.User.DisplayName.ToLower().Contains(queryParams.DisplayName.ToLower()));
+            }
+
+            if (queryParams.MinDifficulty.HasValue)
+            {
+                query = query.Where(t => t.DifficultyRating >= queryParams.MinDifficulty.Value);
+            }
+
+            if (queryParams.MaxDifficulty.HasValue)
+            {
+                query = query.Where(t => t.DifficultyRating <= queryParams.MaxDifficulty.Value);
+            }
+
+            if (queryParams.MinDurationMinutes.HasValue)
+            {
+                query = query.Where(t => t.DurationMinutes >= queryParams.MinDurationMinutes.Value);
+            }
+
+            if (queryParams.MaxDurationMinutes.HasValue)
+            {
+                query = query.Where(t => t.DurationMinutes <= queryParams.MaxDurationMinutes.Value);
+            }
+
+            if (queryParams.ExerciseNames != null && queryParams.ExerciseNames.Count > 0)
+            {
+                var lowerExerciseNames = queryParams.ExerciseNames.Select(e => e.ToLower()).ToList();
+                query = query.Where(t => t.Exercises.Any(te => lowerExerciseNames.Contains(te.Exercise.Name.ToLower())));
+            }
+
+            if (queryParams.MuscleGroups != null && queryParams.MuscleGroups.Count > 0)
+            {
+                var lowerMuscleGroups = queryParams.MuscleGroups.Select(mg => mg.ToLower()).ToList();
+                query = query.Where(t => t.Exercises.Any(te =>
+                    te.Exercise.MuscleGroupsLinks.Any(mgl =>
+                        lowerMuscleGroups.Contains(mgl.MuscleGroup.ToString().ToLower()))));
+            }
+
+            // TODO: Sorting
+
+
+
+
             var dtoQuery = query.ProjectTo<TrainingSessionReadDto>(_mapper.ConfigurationProvider);  
-                
-            // Apply filtering or sorting 
 
             var paginatedList = await PaginatedList<TrainingSessionReadDto>.CreateAsync(dtoQuery, pageIndex, pageSize);
 
