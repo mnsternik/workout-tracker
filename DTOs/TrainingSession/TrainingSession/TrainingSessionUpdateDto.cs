7ac8031869e5e6cf646077335611f986332e95bc
Marcin Sternik <mnsternik@gmail.com>
2025-05-29 20:10:23 +0200

    TrainingSessionUpdateDto created

---
+++ DTOs/TrainingSession/TrainingSession/TrainingSessionUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WorkoutTracker.Api.DTOs.Training.Exercise;
+
+namespace WorkoutTracker.Api.DTOs.TrainingSession.TrainingSession
+{
+    public class TrainingSessionUpdateDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; init; } = string.Empty;
+
+        [StringLength(1000)]
+        public string? Notes { get; init; }
+
+        [Range(1, 5)]
+        public int? DifficultyRating { get; init; }
+
+        [Range(1, 1440)]
+        public int? EstimatedDurationMinutes { get; init; }
+
+        [Required]
+        public IList<TrainingExerciseCreateDto> Exercises { get; init; } = new List<TrainingExerciseCreateDto>();
+    }
+}
