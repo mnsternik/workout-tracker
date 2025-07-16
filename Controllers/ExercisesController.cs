f76240815b8e4a8c9601f8cb1f6cc82810e1d036
Marcin Sternik <mnsternik@gmail.com>
2025-07-16 03:15:22 +0200

    Database access logic moved to service

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkoutTracker.Api.DTOs.Exercise;
+using WorkoutTracker.Api.Models;
+using WorkoutTracker.Api.Services.Exercises;
+
+namespace WorkoutTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly IExerciseService _exerciseService;
+
+        public ExercisesController(IExerciseService exerciseService)
+        {
+
+            _exerciseService = exerciseService;
+        }
+
+        // GET: api/Exercises
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExerciseReadDto>>> GetExercises([FromQuery] ExerciseQueryParameters queryParams)
+        {
+            return await _exerciseService.GetExercisesAsync(queryParams);
+        }
+
+        // GET: api/Exercises/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExerciseReadDto>> GetExercise(int id)
+        {
+            var exerciseDto = await _exerciseService.GetExerciseAsync(id);      
+            // TODO: Add global exception handling and return NotFound when EntityNotFoundException occur in exerciseService
+            return Ok(exerciseDto);
+        }
+
+        // PUT: api/Exercises/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExercise(int id, ExerciseUpdateDto exerciseDto)
+        {
+            await _exerciseService.UpdateExerciseAsync(id, exerciseDto);
+            return NoContent();
+        }
+
+        // POST: api/Exercises
+        [HttpPost]
+        public async Task<ActionResult<Exercise>> PostExercise(ExerciseCreateDto exerciseDto)
+        {
+            var exercise = await _exerciseService.PostExerciseAsync(exerciseDto);
+            return CreatedAtAction(nameof(GetExercise), new { id = exercise.Id }, exercise);
+        }
+
+        // DELETE: api/Exercises/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExercise(int id)
+        {
+            await _exerciseService.DeleteExercise(id);
+            return NoContent();
+        }
+    }
+}
