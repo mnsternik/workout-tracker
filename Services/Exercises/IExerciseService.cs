7198da52dddb6ae55da68baadd4caffa2d097e04
Marcin Sternik <mnsternik@gmail.com>
2025-07-16 03:15:04 +0200

    Interface created

--- Services/Exercises/IExerciseService.cs
+++ Services/Exercises/IExerciseService.cs
@@ -1,6 +1,14 @@
-﻿namespace WorkoutTracker.Api.Services.Exercises
+﻿using WorkoutTracker.Api.DTOs.Exercise;
+using WorkoutTracker.Api.Utilities;
+
+namespace WorkoutTracker.Api.Services.Exercises
 {
     public interface IExerciseService
     {
+        public Task<PaginatedList<ExerciseReadDto>> GetExercisesAsync(ExerciseQueryParameters queryParams);
+        public Task<ExerciseReadDto> GetExerciseAsync(int id);
+        public Task<ExerciseReadDto> PostExerciseAsync(ExerciseCreateDto exerciseDto);
+        public Task UpdateExerciseAsync(int id, ExerciseUpdateDto exerciseDto);
+        public Task DeleteExercise(int id); 
     }
 }
