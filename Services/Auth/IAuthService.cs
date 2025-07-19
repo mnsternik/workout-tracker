0035435ab12669af7f50e54cad2fa3ce388b18fe
Marcin Sternik <mnsternik@gmail.com>
2025-07-19 02:23:43 +0200

    IAuthService created:

---
+++ Services/Auth/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using WorkoutTracker.Api.DTOs.Auth;
+
+namespace WorkoutTracker.Api.Services.Auth
+{
+    public interface IAuthService
+    {
+        public Task RegisterUserAsync(RegisterDto registerDto);
+        public Task<LoginResponseDto> LoginAsync(LoginDto loginDto);
+        public Task<LoginResponseDto> RefreshTokenAsync(RefreshTokenRequestDto tokenDto);
+        public Task LogoutAsync(RefreshTokenRequestDto tokenDto);
+    }
+}
