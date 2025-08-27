8c37236884fc0f013edc874e5fad42fa1cbaf9a3
Marcin Sternik <mnsternik@gmail.com>
2025-08-27 03:53:22 +0200

    ExerciseBuilder fo tests created

---
+++ tests/WorkoutTracker.Tests/ApiTests/Controllers/ExercisesControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using WorkoutTracker.Api.Controllers;
+using WorkoutTracker.Api.DTOs.Exercise;
+using WorkoutTracker.Api.Services.Exercises;
+using WorkoutTracker.Api.Utilities;
+using WorkoutTracker.Tests.SampleData;
+
+namespace WorkoutTracker.Tests.ApiTests.Controllers
+{
+    public class ExercisesControllerTests
+    {
+        private readonly Mock<IExerciseService> _mockExerciseService;
+        private readonly ExercisesController _exerciseController;
+
+        public ExercisesControllerTests()
+        {
+            _mockExerciseService = new Mock<IExerciseService>();
+            _exerciseController = new ExercisesController(_mockExerciseService.Object);
+        }
+
+        [Fact]
+        public async Task GetExercises_ReturnsOK_WithExercises()
+        {
+            // Arrange
+            var queryParams = new ExerciseQueryParameters();
+            
+            //var exercises = ExercisesSampleData.Get();
+
+
+            //_mockExerciseService.Setup(s => s.GetExercisesAsync(queryParams)).ReturnsAsync(sampleExercises);
+
+
+            // Act
+
+
+            // Assert
+        }
+    }
+}
---
+++ tests/WorkoutTracker.Tests/Builders/ExerciseBuilder.cs
@@ -0,0 +1,127 @@
+﻿using WorkoutTracker.Api.DTOs.Exercise;
+using WorkoutTracker.Api.Models;
+
+namespace WorkoutTracker.Tests.Builders
+{
+    public class ExerciseBuilder
+    {
+        private int _id = 1;
+        private string _name = "Push up";
+        private string _description = "Push-up exercise is a close chain kinetic exercise which improves the joint proprioception, joint stability and muscle co-activation around the shoulder joint.";
+        private string? _imageUrl = null;
+        private ExerciseType _exerciseType = ExerciseType.Strength;
+        private ICollection<ExerciseMuscleGroupLink> _muscleGroupsLinks = new List<ExerciseMuscleGroupLink>();
+        private Equipment _equipment = Equipment.None;
+        private DifficultyLevel _difficultyLevel = DifficultyLevel.Intermediate;
+
+        public ExerciseBuilder WithId(int id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public ExerciseBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public ExerciseBuilder WithDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+
+        public ExerciseBuilder WithImageUrl(string imageUrl)
+        {
+            _imageUrl = imageUrl;
+            return this;
+        }
+        public ExerciseBuilder WithExerciseType(ExerciseType type)
+        {
+            _exerciseType = type;
+            return this;
+        }
+
+        public ExerciseBuilder WithMuscleGroups(params MuscleGroup[] groups)
+        {
+            _muscleGroupsLinks = groups
+                .Select(mg => new ExerciseMuscleGroupLink { MuscleGroup = mg })
+                .ToList();
+            return this;
+        }
+
+        public ExerciseBuilder WithEquipment(Equipment equipment)
+        {
+            _equipment = equipment;
+            return this;
+        }
+
+        public ExerciseBuilder WithDifficultyLevel(DifficultyLevel level)
+        {
+            _difficultyLevel = level;
+            return this;
+        }
+
+        public Exercise BuildDomain()
+        {
+            var exercise = new Exercise
+            {
+                Id = _id,
+                Name = _name,
+                Description = _description,
+                ImageUrl = _imageUrl,
+                ExerciseType = _exerciseType,
+                Equipment = _equipment,
+                DifficultyLevel = _difficultyLevel,
+            };
+
+            foreach (var link in _muscleGroupsLinks)
+            {
+                link.Exercise = exercise;
+                link.ExerciseId = exercise.Id;
+                exercise.MuscleGroupsLinks.Add(link);
+            }
+
+            return exercise;
+        }
+
+
+        public ExerciseReadDto BuildReadDto()
+        {
+            var exercise = new ExerciseReadDto
+            {
+                Id = _id,
+                Name = _name,
+                Description = _description,
+                ImageUrl = _imageUrl,
+                ExerciseType = _exerciseType,
+                Equipment = _equipment,
+                DifficultyLevel = _difficultyLevel,
+                MuscleGroups = _muscleGroupsLinks
+                    .Select(mgl => new ExerciseMuscleGroupLinkDto { ExerciseId = _id, MuscleGroup = mgl.MuscleGroup })
+                    .ToList()
+            };
+
+            return exercise;
+        }
+
+        public ExerciseCreateDto BuildCreateDto()
+        {
+            var exercise = new ExerciseCreateDto
+            {
+                Name = _name,
+                Description = _description,
+                ImageUrl = _imageUrl,
+                ExerciseType = _exerciseType,
+                Equipment = _equipment,
+                DifficultyLevel = _difficultyLevel,
+                MuscleGroups = _muscleGroupsLinks
+                  .Select(mgl => new ExerciseMuscleGroupLinkDto { ExerciseId = _id, MuscleGroup = mgl.MuscleGroup })
+                  .ToList()
+            };
+
+            return exercise;
+        }
+    }
+}
