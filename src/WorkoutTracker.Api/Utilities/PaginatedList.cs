5d7d700e5d54eb269ee929f5be2fa525b6903db7
Marcin Sternik <mnsternik@gmail.com>
2025-09-24 01:54:45 +0200

    Default parameters for CreateAsync added

--- src/WorkoutTracker.Api/Utilities/PaginatedList.cs
+++ src/WorkoutTracker.Api/Utilities/PaginatedList.cs
@@ -18,7 +18,7 @@ namespace WorkoutTracker.Api.Utilities
         public bool HasPreviousPage => PageIndex > 1;
         public bool HasNextPage => PageIndex < TotalPages;
 
-        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex = 1, int pageSize = 10)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
