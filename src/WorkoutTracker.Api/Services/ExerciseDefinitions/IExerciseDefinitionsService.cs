3c251ac278b135e9883dab87d2c4d5f439812c51
Marcin Sternik <mnsternik@gmail.com>
2025-09-24 01:58:31 +0200

    Typo fixed

--- src/WorkoutTracker.Api/Services/ExerciseDefinitions/IExerciseDefinitionsService.cs
+++ src/WorkoutTracker.Api/Services/ExerciseDefinitions/IExerciseDefinitionsService.cs
@@ -9,6 +9,6 @@ namespace WorkoutTracker.Api.Services.ExerciseDefinitions
         public Task<ExerciseDefinitionReadDto> GetExerciseAsync(int id);
         public Task<ExerciseDefinitionReadDto> PostExerciseAsync(ExerciseDefinitionCreateDto exerciseDto);
         public Task UpdateExerciseAsync(int id, ExerciseDefinitionUpdateDto exerciseDto);
-        public Task DeleteExercise(int id); 
+        public Task DeleteExerciseAsync(int id); 
     }
 }
