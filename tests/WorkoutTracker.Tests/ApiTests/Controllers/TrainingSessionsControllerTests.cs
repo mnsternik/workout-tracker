670fad43474bf5b7c9ef686e79eebe967a7f5dda
Marcin Sternik <mnsternik@gmail.com>
2025-10-07 13:00:13 +0200

    New test added

--- tests/WorkoutTracker.Tests/ApiTests/Controllers/TrainingSessionsControllerTests.cs
+++ tests/WorkoutTracker.Tests/ApiTests/Controllers/TrainingSessionsControllerTests.cs
@@ -1,8 +1,11 @@
 ﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Security.Claims;
 using WorkoutTracker.Api.Controllers;
 using WorkoutTracker.Api.DTOs.TrainingSession;
+using WorkoutTracker.Api.Exceptions;
 using WorkoutTracker.Api.Models;
 using WorkoutTracker.Api.Services.TrainingSessions;
 using WorkoutTracker.Api.Utilities;
@@ -19,6 +22,25 @@ namespace WorkoutTracker.Tests.ApiTests.Controllers
         {
             _tsServiceMock = new Mock<ITrainingSessionsService>();
             _tsController = new TrainingSessionsController(_tsServiceMock.Object);
+        }
+
+        private string SimulateUser()
+        {
+            string userId = Guid.NewGuid().ToString();
+            var userClaims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+            };
+
+            var userIdentity = new ClaimsIdentity(userClaims);
+            var userPrincipals = new ClaimsPrincipal(userIdentity);
+
+            _tsController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = userPrincipals }
+            };
+
+            return userId;
         }
 
         [Fact]
@@ -57,8 +79,111 @@ namespace WorkoutTracker.Tests.ApiTests.Controllers
             returnedData.PageIndex.Should().Be(pageNumber);
             returnedData.TotalPages.Should().Be((totalItems + pageSize - 1) / pageSize);
             returnedData.Should().HaveCount(pageSize);
+            returnedData.Should().BeEquivalentTo(paginatedList);
         }
 
+        [Fact]
+        public async Task GetTrainingSession_ReturnsOk_WithTrainingReadDto()
+        {
+            // Arrange
+            int exerciseId = 1; 
+            var exercise = new TrainingSessionBuilder().WithId(exerciseId).BuildReadDto();
 
+            _tsServiceMock
+                .Setup(s => s.GetTrainingSessionAsync(exerciseId))
+                .ReturnsAsync(exercise);
+
+            // Act
+            var actionResult = await _tsController.GetTrainingSession(exerciseId);
+
+            // Assert
+            var okResult = actionResult.Result.Should().BeOfType<OkObjectResult>().Subject;
+            okResult.Value.Should().BeEquivalentTo(exercise);
+
+            _tsServiceMock.Verify(s => s.GetTrainingSessionAsync(exerciseId), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetTrainingSession_ThrowsError_WhenIdNotFound()
+        {
+            // Arrange
+            int id = 1;
+            string expectedErrorMessage = $"Entity 'TrainingSession' with ID '{id}' not found.";
+
+            _tsServiceMock
+                .Setup(s => s.GetTrainingSessionAsync(id))
+                .ThrowsAsync(new EntityNotFoundException(nameof(TrainingSession), id));
+
+            // Act
+            Func<Task> act = async () => await _tsController.GetTrainingSession(id);
+
+            // Assert
+            await act.Should().ThrowAsync<EntityNotFoundException>()
+                .WithMessage(expectedErrorMessage);
+
+            _tsServiceMock.Verify(s => s.GetTrainingSessionAsync(id), Times.Once());
+        }
+
+        [Fact]
+        public async Task PostTrainingSession_ReturnsCreatedAtAction_WithLocation()
+        {
+            // Arrange
+            string userId = SimulateUser();
+            var tsCreateDto = new TrainingSessionBuilder().BuildCreateDto();
+            var tsReadDto = new TrainingSessionBuilder().BuildReadDto();
+
+            _tsServiceMock
+                .Setup(s => s.PostTrainingSessionAsync(userId, tsCreateDto))
+                .ReturnsAsync(tsReadDto);
+
+            // Act
+            var actionResult = await _tsController.PostTrainingSession(tsCreateDto);
+
+            // Assert
+            var createdAtActionResult = actionResult.Result.Should().BeOfType<CreatedAtActionResult>().Subject;
+            createdAtActionResult.Value.Should().BeEquivalentTo(tsReadDto);
+
+            createdAtActionResult.ActionName.Should().Be(nameof(_tsController.GetTrainingSession));
+            createdAtActionResult.RouteValues?["id"].Should().Be(tsReadDto.Id);
+        }
+
+        [Fact]
+        public async Task PutTrainingSession_ReturnsNoContent()
+        {
+            // Arrange
+            int tsId = 1;
+            string userId = SimulateUser();
+            var tsUpdateDto = new TrainingSessionBuilder().WithId(tsId).BuildUpdateDto();
+
+            _tsServiceMock
+                .Setup(s => s.UpdateTrainingSessionAsync(tsId, userId, tsUpdateDto))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var actionResult = await _tsController.PutTrainingSession(tsId, tsUpdateDto);
+
+            // Assert
+            actionResult.Should().BeOfType<NoContentResult>();
+            _tsServiceMock.Verify(s => s.UpdateTrainingSessionAsync(tsId, userId, tsUpdateDto), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteTrainingSession_ReturnsNoContent()
+        {
+            // Arrange
+            string userId = SimulateUser();
+            int tsId = 1;
+
+            _tsServiceMock
+                .Setup(s => s.DeleteTrainingSession(tsId, userId))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var actionResult = await _tsController.DeleteTrainingSession(tsId);
+
+            // Assert
+            actionResult.Should().BeOfType<NoContentResult>();
+            _tsServiceMock.Verify(s => s.DeleteTrainingSession(tsId, userId), Times.Once);
+        }
     }
 }
